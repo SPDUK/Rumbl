f76e8ec2fb5dd023185201aee1f2f350875f3e68
SPDUK <spdevuk@gmail.com>
2019-01-28 14:41:08 +0000

    chapter 5: changing auth to use a plug and adding login on create

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -17,4 +17,19 @@ defmodule RumblWeb.Auth do
     user = user_id && Accounts.get_user(user_id)
     assign(conn, :current_user, user)
   end
+
+  @doc """
+  The Plug.Conn struct has a field called assigns. We call setting a
+  value in that structure an assign. Our function stores the given user as the
+  :current_user assign, puts the user ID in the session, and finally configures the
+  session, setting the :renew option to true.
+
+  renew: true  stops session fixations attacks.
+  """
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
 end
--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -3,15 +3,17 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
-  def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
+  # no need to check if halted, plugs automatically check between every plug action, if halted then it just returns the current connection, else it will continue
 
-      conn ->
-        users = Accounts.list_users()
-        render(conn, "index.html", users: users)
-    end
+  # When we changed the index action, we had to explicitly check if the connection halted
+  # or not, before acting on it. Plug pipelines explicitly check for halted: true between
+  # every plug invocation, so the halting concern is neatly solved by Plug.
+
+  plug :authenticate when action in [:index, :show]
+
+  def index(conn, _params) do
+    users = Accounts.list_users()
+    render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
@@ -24,10 +26,14 @@ defmodule RumblWeb.UserController do
     render(conn, "new.html", changeset: changeset)
   end
 
+  @doc """
+  Creates an account and instantly logs it in.
+  """
   def create(conn, %{"user" => user_params}) do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
+        |> RumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} Created!")
         |> redirect(to: Routes.user_path(conn, :index))
 
@@ -39,7 +45,7 @@ defmodule RumblWeb.UserController do
   # If there’s a current user, we return the connection unchanged. Otherwise we
   # store a flash message and redirect back to our application root. We use halt(conn)
   # to stop any downstream transformations.
-  defp authenticate(conn) do
+  defp authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
