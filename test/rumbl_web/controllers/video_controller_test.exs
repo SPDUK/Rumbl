994fd49d30add15a9db328ef9a0115fb0afec6c9
SPDUK <spdevuk@gmail.com>
2019-01-30 17:13:11 +0000

    Adding more video_controller tests

--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -1,7 +1,77 @@
 defmodule RumblWeb.VideoControllerTest do
   use RumblWeb.ConnCase
 
-  # logged out users
+  alias Rumbl.Multimedia
+
+  @create_attrs %{url: "http://youtu.be", title: "vid", description: "a vid"}
+  @invalid_attrs %{title: "invalid"}
+
+  defp video_count, do: Enum.count(Multimedia.list_videos())
+
+  describe "with a logged-in user" do
+    setup %{conn: conn, login_as: username} do
+      user = user_fixture(username: username)
+      conn = assign(conn, :current_user, user)
+
+      {:ok, conn: conn, user: user}
+    end
+
+    @tag login_as: "max"
+    test "lists all user's videos on index", %{conn: conn, user: user} do
+      user_video = video_fixture(user, title: "funny cats")
+      other_video = video_fixture(user_fixture(username: "other"), title: "another video")
+
+      conn = get(conn, Routes.video_path(conn, :index))
+      assert html_response(conn, 200) =~ ~r/Listing Videos/
+      assert String.contains?(conn.resp_body, user_video.title)
+      refute String.contains?(conn.resp_body, other_video.title)
+    end
+
+    @tag login_as: "max"
+    test "creates user video and redirects", %{conn: conn, user: user} do
+      create_conn = post conn, Routes.video_path(conn, :create), video: @create_attrs
+
+      assert %{id: id} = redirected_params(create_conn)
+      assert redirected_to(create_conn) == Routes.video_path(create_conn, :show, id)
+
+      conn = get(conn, Routes.video_path(conn, :show, id))
+      assert html_response(conn, 200) =~ "Show Video"
+
+      assert Multimedia.get_video!(id).user_id == user.id
+    end
+
+    @tag login_as: "max"
+    test "does not create video and renders errors when invalid", %{conn: conn} do
+      count_before = video_count()
+      conn = post conn, Routes.video_path(conn, :create), video: @invalid_attrs
+      assert html_response(conn, 200) =~ "check the errors"
+      assert video_count() == count_before
+    end
+  end
+
+  test "authorizes actions against access by other users", %{conn: conn} do
+    owner = user_fixture(username: "owner")
+    video = video_fixture(owner, @create_attrs)
+    non_owner = user_fixture(username: "sneaky")
+    conn = assign(conn, :current_user, non_owner)
+
+    assert_error_sent :not_found, fn ->
+      get(conn, Routes.video_path(conn, :show, video))
+    end
+
+    assert_error_sent :not_found, fn ->
+      get(conn, Routes.video_path(conn, :edit, video))
+    end
+
+    assert_error_sent :not_found, fn ->
+      put(conn, Routes.video_path(conn, :update, video, video: @create_attrs))
+    end
+
+    assert_error_sent :not_found, fn ->
+      delete(conn, Routes.video_path(conn, :delete, video))
+    end
+  end
+
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each(
       [
@@ -18,34 +88,5 @@ defmodule RumblWeb.VideoControllerTest do
         assert conn.halted
       end
     )
-  end
-
-  describe "with a logged-in user" do
-    setup %{conn: conn, login_as: username} do
-      user = user_fixture(username: username)
-      conn = assign(conn, :current_user, user)
-
-      {:ok, conn: conn, user: user}
-    end
-
-    # we’ll use tags anywhere we want to mark attributes for a
-    # block of tests and describes to scope setups to a block of tests.
-
-    # The tag module attribute accepts a keyword list or an atom. Passing an atom
-    # is a shorthand way to set flag style options. For example @tag :logged_in is
-    # equivalent to @tag logged_in: true. We rewrite our setup block to grab the config map,
-    # which holds our metadata with the conn and tags which we use to populate our user fixture.
-
-    # login_as is matched during setup as username
-    @tag login_as: "max"
-    test "lists all user's videos on index", %{conn: conn, user: user} do
-      user_video = video_fixture(user, title: "funny cats")
-      other_video = video_fixture(user_fixture(username: "other"), title: "another video")
-
-      conn = get(conn, Routes.video_path(conn, :index))
-      assert html_response(conn, 200) =~ ~r/Listing Videos/
-      assert String.contains?(conn.resp_body, user_video.title)
-      refute String.contains?(conn.resp_body, other_video.title)
-    end
   end
 end
