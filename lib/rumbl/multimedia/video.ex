3637d17cb9825d92f4c004c23169529f26fd819c
SPDUK <spdevuk@gmail.com>
2019-01-30 11:55:58 +0000

    adding constraint to videos/categories assoc

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -15,10 +15,15 @@ defmodule Rumbl.Multimedia.Video do
     timestamps()
   end
 
-  @doc false
+  @doc """
+  That assoc_constraint converts foreign-key constraint errors into human-readable
+  error messages and guarantees that a video is created only if the category
+  exists in the database.
+  """
   def changeset(video, attrs) do
     video
     |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> assoc_constraint(:category)
   end
 end
