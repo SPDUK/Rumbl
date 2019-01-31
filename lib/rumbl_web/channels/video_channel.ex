01102694bfc8c06e73d17993e299e1646d632d7b
SPDUK <spdevuk@gmail.com>
2019-01-31 16:39:43 +0000

    adding broadcasting for an annotation

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -11,33 +11,42 @@ defmodule RumblWeb.VideoChannel do
   add the video ID from our topic to socket.assigns.
   """
   def join("videos:" <> video_id, _params, socket) do
-    :timer.send_interval(5_000, :ping)
     {:ok, socket}
   end
 
   @doc """
-  The handle_info callback is invoked whenever an Elixir message reaches the
-  channel. In this case, we match on the periodic :ping message and increase a
-  counter every time it arrives.
-
-
-  Our handle_info takes our socket, takes the existing count (or a default of
-  1), and increases that count by one. We then return a tagged tuple. :noreply
-  means we’re not sending a reply, and the assign function transforms our
-  socket by adding the new count. Conceptually, we’re taking a socket and
-  returning a transformed socket. This implementation bumps the count in
-  :assigns by one, each time it’s called.
-
-
-
-  handle_info is basically a loop. Each time, it returns the socket as the last tuple
-  element for all callbacks. This way, we can maintain state. We simply push the ping event,
-  and the JavaScript client picks up these events with the channel.on(event, callback) API.
+  This function will handle all incoming messages to a channel, pushed
+  directly from the remote client.
   """
-  def handle_info(:ping, socket) do
-    count = socket.assigns[:count] || 1
-    push(socket, "ping", %{count: count})
+  def handle_in("new_annotation", params, socket) do
+    broadcast!(socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    })
 
-    {:noreply, assign(socket, :count, count + 1)}
+    {:reply, :ok, socket}
   end
 end
+
+# The handle_info callback is invoked whenever an Elixir message reaches the
+# channel. In this case, we match on the periodic :ping message and increase a
+# counter every time it arrives.
+
+# Our handle_info takes our socket, takes the existing count (or a default of
+# 1), and increases that count by one. We then return a tagged tuple. :noreply
+# means we’re not sending a reply, and the assign function transforms our
+# socket by adding the new count. Conceptually, we’re taking a socket and
+# returning a transformed socket. This implementation bumps the count in
+# :assigns by one, each time it’s called.
+
+# handle_info is basically a loop. Each time, it returns the socket as the last tuple
+# element for all callbacks. This way, we can maintain state. We simply push the ping event,
+# and the JavaScript client picks up these events with the channel.on(event, callback) API.
+
+# def handle_info(:ping, socket) do
+#   count = socket.assigns[:count] || 1
+#   push(socket, "ping", %{count: count})
+
+#   {:noreply, assign(socket, :count, count + 1)}
+# end
